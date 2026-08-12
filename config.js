db310301d3eeb1df9700cb9cbfdce5a913d61eee
Shabiir <shabiir@gmail.com>
2026-08-11 20:26:44 -0500

    Connect Mumbai RSVP backend
    Replace the archived Chicago Apps Script URL with the Mumbai-only deployment.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Paste the Apps Script /exec deployment URL between the quotes.
 window.RSVP_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbzhOHqL8-4eObxGbqQ6g_hbFnKQXW4bNlkHRtOWg4FLQYoPa6sUFiqn7mjk9gZHIIqo/exec"
+  apiUrl: "https://script.google.com/macros/s/AKfycbxmzsEtNUwPtotS0p2XrKvpxXbLzr6im4evAgPIPY_oR5i14yLygMZXPCbvL4VDlhQdYQ/exec"
 };
